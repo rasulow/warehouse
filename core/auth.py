f1c512158b2169fd79ba603e6e4d0ff3a006a0b7
rasulow <rasulov.olympusss@gmail.com>
2023-11-25 21:27:22 +0500

    commit

--- core/auth.py
+++ core/auth.py
@@ -11,7 +11,7 @@ from fastapi.security import OAuth2PasswordRequestForm, OAuth2PasswordBearer
 from jose import jwt, JWTError
 from dotenv import load_dotenv
 import os
-from core import admin_rbac, user_dependency
+
 
 router = APIRouter(
     prefix='/auth',
@@ -45,10 +45,8 @@ class Token(BaseModel):
 db_dependency = Annotated[Session, Depends(get_db)]
 
 
-@router.post('/', status_code=status.HTTP_201_CREATED, summary='ADMIN')
-@admin_rbac
+@router.post('/', status_code=status.HTTP_201_CREATED)
 async def create_user(db: db_dependency,
-                      user: user_dependency,
                       create_user_request: CreateUserRequest):
     create_user_model = User(
         username=create_user_request.username,
